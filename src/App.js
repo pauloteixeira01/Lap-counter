416e48c0c1344e86540ef28364e5b8c56de4d93c
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-05-29 15:29:33 -0300

    Terminei o contador de voltas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import MostraVoltas from './MostraVoltas'
+import MostraTempo from './MostraTempo'
+import Button from './Button'
+import './style.css'
 
-function App() {
+
+function App () {
+  const [numVoltas, setNumVoltas] = useState(0)
+  const [running, setRunning] = useState(false)
+  const [tempo, setTempo] = useState(0)
+
+  //Chama o setInterval apenas uma vez.
+  useEffect(() => {
+    let timer = null
+    if(running){
+      timer = setInterval(() => {
+        setTempo(old => old + 1)
+      }, 1000)
+    }
+    return () => {
+      if(timer) {
+        clearInterval(timer)
+      }
+    }
+  }, [running])
+
+  const toggleRunning = () => {
+    setRunning(!running)
+  }
+
+
+  const increment = () => {
+    setNumVoltas(numVoltas + 1) 
+  }
+  const decrement = () => {
+    if (numVoltas > 0) {
+      setNumVoltas(numVoltas - 1)
+    }
+    
+  }
+  const reset = () => {
+    setNumVoltas(0)
+    setTempo(0)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MostraVoltas voltas={numVoltas} />
+      <Button text='+' className='bigger' onClick={increment} />
+      <Button text='-' className='bigger' onClick={decrement} />
+      {
+        numVoltas > 0 &&
+        <MostraTempo tempo={Math.round(tempo/numVoltas)} />
+      }
+      <Button onClick={toggleRunning} text={running ? 'Pausar' : 'Iniciar'} />
+      <Button onClick={reset} text='Reiniciar' />
     </div>
   );
 }
 
-export default App;
+export default App
